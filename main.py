3e99b9eb6a1e49f61effd18f4c1f4de0f4f068b8
eke333 <ehoumanjoel.fr@gmail.com>
2024-09-07 07:30:56 +0000

    initial commit
    Création du projet avec un message "Hiii !!! ...)

---
+++ main.py
@@ -0,0 +1,15 @@
+# This is a sample Python script.
+
+# Press Maj+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def hello(name):
+    print(f'\nHi !!!, {name}')
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    hello('\n\nI\'m a project created for using my apk\nin the python API linked to the project\nPerformanceQSE belongs the firm Vision & Stratégie Groupe.')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
